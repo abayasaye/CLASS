d5a9a63ef7c2b73d67447d1e7c80b2059fafc430
abayasaye <abay400@gmail.com>
2022-07-15 18:46:20 +0300

    n

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
-class Bulding {
-    constructor(city,street,floor,numberOfRooms){
-        this.city = city;
-        this.sreet = street;
-        this.floor = floor;
-        this.numberOfRooms = numberOfRooms;
+// class Bulding {
+//     constructor(city,street,floor,numberOfRooms){
+//         this.city = city;
+//         this.sreet = street;
+//         this.floor = floor;
+//         this.numberOfRooms = numberOfRooms;
 
 
-    }
-}
-const bulding = new Bulding( haifa , sholomo , 5 , 5 )
+//     }
+// }
+// const bulding = new Bulding( haifa , sholomo , 5 , 5 )
 
-class Villa extends bulding {
-    constructor(city,street,floor,numberOfRooms ,poolSize , yardSize ,){
-        super(city, street,floor , numberOfRooms)
-        this.poolSize = poolSize;
-        this.yardSize = yardSize;
+// class Villa extends bulding {
+//     constructor(city,street,floor,numberOfRooms ,poolSize , yardSize ,){
+//         super(city, street,floor , numberOfRooms)
+//         this.poolSize = poolSize;
+//         this.yardSize = yardSize;
 
-    }
-}
-const newVilla = new Villa (haifa , david , 0 ,8, 100, 40) 
-console.log(Villa);
+//     }
+// }
+// const newVilla = new Villa (haifa , david , 0 ,8, 100, 40) 
+// console.log(Villa);
 
 class Movies {
-    constructor(year , ){
+    constructor(year , synopsis ,title,rating ){
+        this.year = year;
+        this.synopsis =synopsis;
+        this.title =title;
+        this.rating = rating;
+    }
+    getMoviesDetailes(){
+        return fetch("popcorn-api/src/models/Movie.js")
+        .then(res=>res.json())
+        .then(result=>console.log(result))
 
     }
+    
 }
